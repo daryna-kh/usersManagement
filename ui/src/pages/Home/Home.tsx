a2f56ada0652078dd462fd1308642dfbb155b069
Daryna <rinabkh98@gmail.com>
2025-05-07 13:39:04 +0200

    ui fixes #3

--- ui/src/pages/Home/Home.tsx
+++ ui/src/pages/Home/Home.tsx
@@ -1,10 +1,26 @@
 import { Button, Stack, TextField } from "@mui/material";
 
 export const Home = () => {
+  const fetchItem = async () => {
+    try {
+      const responce = await fetch("/api/items", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      return responce;
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
   return (
     <Stack spacing={2}>
       <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <Button variant="contained">Contained</Button>
+      <Button variant="contained" onClick={() => fetchItem()}>
+        Contained
+      </Button>
     </Stack>
   );
 };
